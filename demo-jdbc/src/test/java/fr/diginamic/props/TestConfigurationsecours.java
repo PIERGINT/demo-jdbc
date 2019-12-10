60484230bc6c396944d7d82e373a803cd04d1634
PIERGIT <p.marget@yahoo.com>
2019-12-10 15:59:40 +0100

    TP2 properties 10 12 19

---
+++ demo-jdbc/src/test/java/fr/diginamic/props/TestConfigurationsecours.java
@@ -0,0 +1,33 @@
+
+	package fr.diginamic.props;
+
+	import java.util.ResourceBundle;
+	import java.util.Set;
+
+	public class TestConfigurationsecours {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			ResourceBundle cle1 = ResourceBundle.getBundle("Pierredatafiles");
+			String driverName = cle1.getString("Pierre.driver");
+			
+			System.out.println("drivername");
+			
+			ResourceBundle http = ResourceBundle.getBundle("Pierre");
+			String adresse = http.getString("Pierre.url");
+			
+			System.out.println("adresse");
+			
+			Set<String> valeurs= cle1.keySet();
+			for (String val: valeurs) {
+				
+				driverName = cle1.getString(val);
+				
+				System.out.println(driverName);
+			}
+		}
+		
+
+	}
+
+
