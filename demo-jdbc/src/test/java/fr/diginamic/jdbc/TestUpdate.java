d842baabffff0df9fc877bd9a8d3e147b879a30b
PIERGIT <p.marget@yahoo.com>
2019-12-11 16:45:38 +0100

    TP jdbc classe Update

---
+++ demo-jdbc/src/test/java/fr/diginamic/jdbc/TestUpdate.java
@@ -0,0 +1,55 @@
+package fr.diginamic.jdbc;
+
+import java.sql.DriverManager;
+import org.mariadb.jdbc.Driver;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.ResourceBundle;
+import java.sql.Statement;
+
+public class TestUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			System.out.println("Driver chargé");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			System.out.println("Unloaded");
+		}
+		
+		ResourceBundle cle1 = ResourceBundle.getBundle("Pierredatafiles");
+		String urlName = cle1.getString("Pierredatafiles.url");
+	
+		ResourceBundle cle2 = ResourceBundle.getBundle("Pierredatafiles");
+		String userName = cle2.getString("Pierredatafiles.user");
+		
+		ResourceBundle cle3 = ResourceBundle.getBundle("Pierredatafiles");
+		String password = cle3.getString("Pierredatafiles.password");
+		
+		try {
+		Connection connect = DriverManager.getConnection(urlName,userName,password);
+		
+		System.out.println("Connected");
+		
+		Statement perState = connect.createStatement();
+		int nb = perState.executeUpdate("UPDATE FOURNISSEUR SET NOM='La Maison des Peintures'WHERE ID=4");
+				
+		connect.close();
+		
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		
+		System.out.println("Connection failed");
+		
+		
+	}
+}
+}
+	
